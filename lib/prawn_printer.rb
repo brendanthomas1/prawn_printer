7b0b9130a94633b91cff79cb56ae31cc549373be
Brendan Thomas <bt331609@gmail.com>
2017-11-10 11:51:55 -0500

    print printables

--- lib/prawn_printer.rb
+++ lib/prawn_printer.rb
@@ -6,9 +6,20 @@ module PrawnPrinter
   class Printer
     attr_reader :document
 
-    def initialize(orientation: :portrait)
+    def initialize(orientation: :portrait, positionables: [])
       @document = Prawn::Document.new page_layout: orientation,
                                       page_size: "LETTER"
+      print_printables(printables)
+    end
+
+    private
+
+    def print(printable)
+      @document.formatted_text_box(printable.text)
+    end
+
+    def print_printables(printables)
+      printables.each(&method(:print))
     end
   end
 end
