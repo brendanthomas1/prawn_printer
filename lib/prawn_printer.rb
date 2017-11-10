c4f38f8dadaf0ee18f4dc0b380e3c9e659dcf6c7
Brendan Thomas <bt331609@gmail.com>
2017-11-10 15:12:43 -0500

    add options for printables

--- lib/prawn_printer.rb
+++ lib/prawn_printer.rb
@@ -6,7 +6,7 @@ module PrawnPrinter
   class Printer
     attr_reader :document
 
-    def initialize(orientation: :portrait, positionables: [])
+    def initialize(orientation: :portrait, printables: [])
       @document = Prawn::Document.new page_layout: orientation,
                                       page_size: "LETTER"
       print_printables(printables)
@@ -14,8 +14,29 @@ module PrawnPrinter
 
     private
 
+    def additional_options(printable)
+      custom_options = printable.fetch(:options, {})
+      default_options.merge(custom_options)
+    end
+
+    def color(printable)
+      printable.fetch(:color, '000000')
+    end
+
+    def default_options
+      { valign: :center, size: 12, height: 18, width: 300,
+        overflow: :shrink_to_fit, min_font_size: 8, single_line: true }
+    end
+
+    def get_coords(printable)
+      [printable.fetch(:x, 100), printable.fetch(:y, 100)]
+    end
+
     def print(printable)
-      @document.formatted_text_box(printable.text)
+      @document.formatted_text_box(
+        [{ text: printable[:text], color: color(printable) }],
+        at: get_coords(printable), **additional_options(printable)
+      )
     end
 
     def print_printables(printables)
